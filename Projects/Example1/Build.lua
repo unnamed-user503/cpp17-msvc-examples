3d969b4a4e2f7fc2c0cea2d8b55406e527f3d423
unnamed-user503 <undefined.u_u.404notfound@outlook.com>
2024-02-04 05:59:31 +0900

    first commit

---
+++ Projects/Example1/Build.lua
@@ -0,0 +1,32 @@
+project "Example1"
+    kind          "ConsoleApp" -- ConsoleApp WindowedApp SharedLib StaticLib
+    language      "C++"
+    cppdialect    "C++17"
+    staticruntime "off"
+    characterset  "MBCS"
+
+    pchheader "Pch.hpp"
+    pchsource "Source/Pch.cpp"
+
+    targetdir ("%{wks.location}/Binaries/" .. OutputDir .. "/%{prj.name}")
+    objdir    ("%{wks.location}/Binaries-Intermediates/" .. OutputDir .. "/%{prj.name}")
+
+    vspropertysettings
+    {
+        VcpkgTriplet = "x64-windows-static",
+        VcpkgEnabled = "true",
+    }
+
+    files
+    {
+        "Example1.exe.manifest", "Source/**.cpp", "Source/**.hpp", "Source/**.h",
+    }
+
+    includedirs
+    {
+        "Source",
+    }
+
+    links
+    {
+    }
---
+++ Projects/Example1/Source/Main.cpp
@@ -0,0 +1,8 @@
+#include "Pch.hpp"
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
